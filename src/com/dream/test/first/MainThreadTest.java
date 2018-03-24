0e1db367ee98efdecd8921b02944ed77d78ef194
liuxinjie123 <jackcodejs@gmail.com>
2018-03-24 21:26:14 +0800

    main thread test

---
+++ src/com/dream/test/first/MainThreadTest.java
@@ -0,0 +1,10 @@
+package com.dream.test.first;
+
+public class MainThreadTest {
+    public static void main(String[] args) {
+        System.out.println(Thread.currentThread().getName());
+        System.out.println(Thread.currentThread().getId());
+        System.out.println(Thread.currentThread().getPriority());
+        System.out.println(Thread.currentThread().toString());
+    }
+}

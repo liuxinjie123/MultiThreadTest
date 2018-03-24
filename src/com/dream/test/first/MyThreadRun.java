09a08d388b712d59a76d91e7a63358671ea5f702
liuxinjie123 <jackcodejs@gmail.com>
2018-03-24 21:53:07 +0800

    test

--- src/com/dream/test/first/MyThreadRun.java
+++ src/com/dream/test/first/MyThreadRun.java
@@ -2,6 +2,7 @@ package com.dream.test.first;
 
 public class MyThreadRun {
     public static void main(String[] args) {
+        System.out.println(" --- start --- ");
         MyThread thread = new MyThread();
         thread.start();
         System.out.println(" --- end --- ");
